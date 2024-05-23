92b995e22978f3566f6a67d4a189bdbe1e657ac2
Quynh Le <lesatthuga@gmail.com>
2024-05-23 16:06:55 +0700

    fix bug

--- LittleLemon/LittleLemon/wsgi.py
+++ LittleLemon/LittleLemon/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for LittleKemon project.
+WSGI config for LittleLemon project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LittleKemon.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LittleLemon.settings')
 
 application = get_wsgi_application()
